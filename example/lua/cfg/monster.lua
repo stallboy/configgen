b0916be4e5b69be8d9fa0365d469b4351ba50ba3
cheng xiaosan <stallboy@qq.com>
2019-01-22 12:27:13 +0800

    lua生成代码优化

--- example/lua/cfg/monster.lua
+++ example/lua/cfg/monster.lua
@@ -8,7 +8,9 @@ local mk = cfg._mk.table(this, { { "all", "get", 1 }, }, nil, nil,
     "posList"  -- list,Position
     )
 
-mk(1, {Beans.position(1, 2, 3), Beans.position(11, 22, 33), Beans.position(111, 222, 333)})
-mk(2, {Beans.position(33, 44, 55)})
+local position = Beans.position
+
+mk(1, {position(1, 2, 3), position(11, 22, 33), position(111, 222, 333)})
+mk(2, {position(33, 44, 55)})
 
 return this
--- example/lua/cfg/task/task.lua
+++ example/lua/cfg/task/task.lua
@@ -12,11 +12,17 @@ local mk = cfg._mk.table(this, { { "all", "get", 1 }, }, nil, nil,
     "exp"  -- int
     )
 
-mk(1, "杀个怪", "杀怪", 2, Beans.task.completecondition.killmonster(1, 3), 1000)
-mk(2, "和npc对话", "和npc对话", 3, Beans.task.completecondition.talknpc(1), 2000)
-mk(3, "收集物品", "收集物品", 0, Beans.task.completecondition.collectitem(11, 1), 3000)
-mk(4, "杀怪并且收集物品", "杀怪并且收集物品", 0, Beans.task.completecondition.conditionand(Beans.task.completecondition.killmonster(1, 3), Beans.task.completecondition.collectitem(11, 1)), 4000)
-mk(5, "杀怪对话并且收集物品", "杀怪对话并且收集物品", 0, Beans.task.completecondition.conditionand(Beans.task.completecondition.conditionand(Beans.task.completecondition.killmonster(1, 3), Beans.task.completecondition.talknpc(1)), Beans.task.completecondition.collectitem(11, 1)), 5000)
-mk(6, "聊天并且杀怪", "测试转义符号", 0, Beans.task.completecondition.conditionand(Beans.task.completecondition.chat("葵花宝典,123"), Beans.task.completecondition.killmonster(1, 3)), 5000)
+local talknpc = Beans.task.completecondition.talknpc
+local conditionand = Beans.task.completecondition.conditionand
+local collectitem = Beans.task.completecondition.collectitem
+local chat = Beans.task.completecondition.chat
+local killmonster = Beans.task.completecondition.killmonster
+
+mk(1, "杀个怪", "杀怪", 2, killmonster(1, 3), 1000)
+mk(2, "和npc对话", "和npc对话", 3, talknpc(1), 2000)
+mk(3, "收集物品", "收集物品", 0, collectitem(11, 1), 3000)
+mk(4, "杀怪并且收集物品", "杀怪并且收集物品", 0, conditionand(killmonster(1, 3), collectitem(11, 1)), 4000)
+mk(5, "杀怪对话并且收集物品", "杀怪对话并且收集物品", 0, conditionand(conditionand(killmonster(1, 3), talknpc(1)), collectitem(11, 1)), 5000)
+mk(6, "聊天并且杀怪", "测试转义符号", 0, conditionand(chat("葵花宝典,123"), killmonster(1, 3)), 5000)
 
 return this
